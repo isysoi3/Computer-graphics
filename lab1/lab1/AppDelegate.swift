8ee252ffc0ae2043be8818b72a027a758257e98d
Ilya Sysoi <14ilya12322@gmail.com>
2019-02-19 21:48:53 +0300

    init project with simple UI

---
+++ lab1/lab1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  lab1
+//
+//  Created by Ilya Sysoi on 2/19/19.
+//  Copyright © 2019 Ilya Sysoi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ lab1/lab1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  lab1
+//
+//  Created by Ilya Sysoi on 2/19/19.
+//  Copyright © 2019 Ilya Sysoi. All rights reserved.
+//
+
+import Cocoa
+import SnapKit
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

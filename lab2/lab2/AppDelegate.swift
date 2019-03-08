42dab3a0c5e9d15cef2583f5af7d99a5620eb9ad
Ilya Sysoi <14ilya12322@gmail.com>
2019-03-08 16:16:54 +0300

    lab2: project init

---
+++ lab2/lab2/AppDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  AppDelegate.swift
+//  lab2
+//
+//  Created by Ilya Sysoi on 3/8/19.
+//  Copyright © 2019 Ilya Sysoi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    @IBAction func openDocument(_ sender: Any) {
+         print("openDocument got called")
+        let dialog = NSOpenPanel();
+        
+        dialog.title                   = "Choose a image file or directory";
+        dialog.showsResizeIndicator    = false;
+        dialog.showsHiddenFiles        = false;
+        dialog.canChooseDirectories    = true;
+        dialog.canCreateDirectories    = false;
+        dialog.allowsMultipleSelection = false;
+        dialog.allowedFileTypes        = ["jpg" , "gif", "tif", "bmp", "png", "pcx"];
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            let result = dialog.url // Pathname of the file
+            
+            if (result != nil) {
+                let path = result!.path
+            }
+        } else {
+            // User clicked on "Cancel"
+            return
+        }
+    }
+    
+    
+}
+
---
+++ lab2/lab2/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  lab2
+//
+//  Created by Ilya Sysoi on 3/8/19.
+//  Copyright © 2019 Ilya Sysoi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
+
+extension ViewController {
+    
+    
+}

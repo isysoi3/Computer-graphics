3df8122d683cf8f7d07f1b808e46221d6c8b182c
Ilya Sysoi <14ilya12322@gmail.com>
2019-03-09 09:56:16 +0300

    reformat file

--- lab2/lab2/AppDelegate.swift
+++ lab2/lab2/AppDelegate.swift
@@ -22,16 +22,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @IBAction func openDocument(_ sender: Any) {
-         print("openDocument got called")
+        print("openDocument got called")
         let dialog = NSOpenPanel();
         
-        dialog.title                   = "Choose a image file or directory";
-        dialog.showsResizeIndicator    = false;
-        dialog.showsHiddenFiles        = false;
-        dialog.canChooseDirectories    = true;
-        dialog.canCreateDirectories    = false;
+        dialog.title = "Choose a image file or directory";
+        dialog.showsResizeIndicator = false;
+        dialog.showsHiddenFiles = false;
+        dialog.canChooseDirectories = true;
+        dialog.canCreateDirectories = false;
         dialog.allowsMultipleSelection = false;
-        dialog.allowedFileTypes        = ["jpg" , "gif", "tif", "bmp", "png", "pcx"];
+        dialog.allowedFileTypes = ["jpg" , "gif", "tif", "bmp", "png", "pcx"];
         
         if (dialog.runModal() == NSApplication.ModalResponse.OK) {
             let result = dialog.url // Pathname of the file

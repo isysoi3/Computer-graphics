550761426e47f0a5b7b1c4c30d1197a08371fb39
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-03-18 08:45:59 +0300

    project inited

---
+++ lab3/lab3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lab3
+//
+//  Created by Ilya Sysoi on 3/18/19.
+//  Copyright © 2019 isysoi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

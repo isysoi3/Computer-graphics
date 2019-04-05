da101d78b36fd96834156c372279668ccea8f67b
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-04-05 08:45:01 +0300

    lab4: init

---
+++ lab4/lab4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  lab4
+//
+//  Created by Ilya Sysoi on 4/5/19.
+//  Copyright © 2019 isysoi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

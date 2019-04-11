38c77cb0ee09d60d9dcc8bc4ca067198b255356e
Ilya Sysoi <14ilya12322@gmail.com>
2019-04-11 22:38:30 +0300

    Revert "tmp commit"
    This reverts commit 2ebde90a390792032a4534070114170fca9803af.

--- lab4/lab4Tests/lab4Tests.swift
+++ lab4/lab4Tests/lab4Tests.swift
@@ -90,7 +90,6 @@ class lab4Tests: XCTestCase {
     
 }
 
-
 /*
 Time for "stepByStep":                  0.0003579854965209961 seconds
 Time for "digitalDifferentialAnalyzer": 0.0003459453582763672 seconds

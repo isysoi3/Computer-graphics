0c3e7a9c4ca0b8efd00ce95138348ed99f92691a
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-04-22 09:01:41 +0300

    project init

---
+++ lab5/lab5/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lab5
+//
+//  Created by Ilya Sysoi on 4/22/19.
+//  Copyright © 2019 Ilya Sysoi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
